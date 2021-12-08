36948244bfa9c03f07d8fe6a128a824a2a26e4d6
Lucas Rodrigues <atakemaximo@gmail.com>
2021-12-08 16:45:19 -0300

    Commit final

--- api/src/api/schemas/usersSchema.js
+++ api/src/api/schemas/usersSchema.js
@@ -2,7 +2,7 @@ const joi = require('joi');
 
 module.exports = joi.object({
   fullname: joi.string().required().max(100),
-  date_birth: joi.date(),
+  date_birth: joi.string(),
   email: joi.string().required().email().max(100),
   city: joi.string().required().max(100),
   school: joi.string().required().max(100),
